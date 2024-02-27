d4542371eb732415619f57e2040f939138a9538f
MD SAYMUM ISLAM SIYAM <71134419+SISIYAM@users.noreply.github.com>
2024-02-27 17:35:04 +0600

    28FebPicoSatTest

--- js/main.js
+++ js/main.js
@@ -3,18 +3,17 @@ $(document).ready(function () {
   setInterval(() => {
     $.ajax({
       type: "GET",
-      url: "OBCSensors.json",
+      url: "OBCTemp.json",
       success: function (response) {
         response.forEach((data) => {
-          $("#batteryTemp_1").html(data["Battery Temperature 1"]);
-          $("#batteryTemp_2").html(data["Battery Temperature 2"]);
-          $("#batteryTemp_3").html(data["Battery Temperature 3"]);
-          $("#batteryTemp_4").html(data["Battery Temperature 4"]);
-          $("#boardTemp").html(data["Board Temperature"]);
+          $("#batteryTemp_1").html(data["Battery Temperature 1"] + "&degC");
+          $("#batteryTemp_2").html(data["Battery Temperature 2"] + "&degC");
+          $("#batteryTemp_4").html(data["Battery Temperature 4"] + "&degC");
+          $("#boardTemp").html(data["Board Temperature"] + "&degC");
         });
       },
     });
-  }, 100);
+  }, 1000);
 
   // fetching data from BME280 sensor
   setInterval(() => {
@@ -23,27 +22,14 @@ $(document).ready(function () {
       url: "BME280.json",
       success: function (result) {
         result.forEach((bme) => {
-          $("#ambientTemp").html(bme["Ambient Temperature"]);
-          $("#data_humidity").html(bme.Humidity);
-          $("#data_pressure").html(bme.pressure);
-          $("#data_altitude").html(bme.altitude);
-          $("#graphHumidity").html(bme.Humidity.toString().replace(/\%/g, ""));
+          $("#ambientTemp").html(bme["Ambient Temperature"] + "&degC");
+          $("#data_humidity").html(bme.Humidity + "%");
+          $("#data_pressure").html(bme.pressure + "hPa");
+          $("#data_altitude").html(bme.altitude + "m");
         });
       },
     });
-  }, 100);
-
-  // fetching image from image.json
-
-  // $.ajax({
-  //   type: "GET",
-  //   url: "image.json",
-  //   success: function (res) {
-  //     res.forEach((element) => {
-  //       $("#image").prepend('<img height="350" src="' + element.path + '" />');
-  //     });
-  //   },
-  // });
+  }, 1000);
 
   setInterval(() => {
     $.ajax({
@@ -55,239 +41,34 @@ $(document).ready(function () {
         });
       },
     });
-  }, 100);
+  }, 1000);
 
-  // insert json data automatically for obc sensor
-  let Temperature_1 = 5;
-  let Temperature_2 = 6;
-  let Temperature_3 = 7;
-  let Temperature_4 = 8;
-  let board_temperature = 4;
+  // // fetching Gyro sensor Data
+  // setInterval(() => {
+  //   $.ajax({
+  //     type: "GET",
+  //     url: "gyroSensor.json",
+  //     success: function (gyroResponse) {
+  //       gyroResponse.forEach((gyroElement) => {
+  //         $("#acceleration_id").html(gyroElement.acceleration);
+  //         $("#gyroscope_id").html(gyroElement.gyroscope);
+  //       });
+  //     },
+  //   });
+  // }, 1000);
 
-  setInterval(() => {
-    Temperature_1 = Temperature_1 + 0.01;
-    Temperature_2 = Temperature_2 + 1;
-    Temperature_3 = Temperature_3 + 4;
-    Temperature_4 = Temperature_4 + 7;
-    board_temperature = board_temperature + 10;
-    $("#temp1").val(Temperature_1.toFixed(2));
-    $("#temp2").val(Temperature_2.toFixed(2));
-    $("#temp3").val(Temperature_3.toFixed(2));
-    $("#temp4").val(Temperature_4.toFixed(2));
-    $("#Boardtemp").val(board_temperature.toFixed(2));
-
-    $.ajax({
-      type: "post",
-      url: "json.php",
-      data: {
-        BatteryTemperature1: Temperature_1.toFixed(2),
-        BatteryTemperature2: Temperature_2.toFixed(2),
-        BatteryTemperature3: Temperature_3.toFixed(2),
-        BatteryTemperature4: Temperature_4.toFixed(2),
-        BoardTemperature: board_temperature.toFixed(2),
-        OBC: "OBC",
-      },
-      success: function (res) {
-        if (res == 200) {
-        } else {
-          console.log(res);
-        }
-      },
-    });
-  }, 2000);
-
-  // insert json data automatically for obc sensor
-  let ambientTemperature = 17;
-  let humidity = 30;
-  let pressure = 800;
-  let altitude = 0;
-
-  setInterval(() => {
-    ambientTemperature = ambientTemperature + 0.01;
-    humidity = humidity + 0.01;
-    pressure = pressure + 0.01;
-    altitude = altitude + 10;
-    $("#ambientTemperature").val(ambientTemperature.toFixed(2));
-    $("#humidity").val(humidity.toFixed(2));
-    $("#pressure").val(pressure.toFixed(2));
-    $("#altitude").val(altitude);
-
-    $.ajax({
-      type: "post",
-      url: "json.php",
-      data: {
-        ambientTemperature: ambientTemperature.toFixed(2),
-        humidity: humidity.toFixed(2),
-        pressure: pressure.toFixed(2),
-        altitude: altitude,
-        BME: "BME",
-      },
-      success: function (resBME) {
-        if (resBME == 200) {
-          console.log("Success");
-        } else {
-          console.log("Failed");
-        }
-      },
-    });
-  }, 2000);
+  // // fetching data from GPS Sensor
+  // setInterval(() => {
+  //   $.ajax({
+  //     type: "GET",
+  //     url: "GPS.json",
+  //     success: function (gpsResponse) {
+  //       gpsResponse.forEach((gpsElement) => {
+  //         $("#latitude_id").html(gpsElement.Latitude);
+  //         $("#longitude_id").html(gpsElement.Longitude);
+  //         $("#gps_altitude").html(gpsElement.gpsAltitude);
+  //       });
+  //     },
+  //   });
+  // }, 1000);
 });
-
-// code for graph
-/*
-function chartApex() {
-  var options1 = {
-    chart: {
-      height: 280,
-      type: "radialBar",
-    },
-    series: [0],
-    colors: ["#124CD2"],
-    plotOptions: {
-      radialBar: {
-        startAngle: -135,
-        endAngle: 135,
-        track: {
-          background: "#333",
-          startAngle: -135,
-          endAngle: 135,
-        },
-        dataLabels: {
-          name: {
-            show: true,
-          },
-          value: {
-            fontSize: "30px",
-            show: true,
-            formatter: function (humidityValue) {
-              return humidityValue + "%";
-            },
-          },
-        },
-      },
-    },
-    fill: {
-      type: "gradient",
-      gradient: {
-        shade: "dark",
-        type: "horizontal",
-        gradientToColors: ["#87D4F9"],
-        stops: [0, 100],
-      },
-    },
-    stroke: {
-      lineCap: "butt",
-    },
-    labels: ["Humidity"],
-  };
-  return options1;
-}
-setInterval(chartApex, 100);
-
-var chart = new ApexCharts(document.querySelector("#chart1"), chartApex());
-chart.render();
-
-var options2 = {
-  chart: {
-    height: 280,
-    type: "radialBar",
-  },
-  series: [0],
-  colors: ["#20E647"],
-  plotOptions: {
-    radialBar: {
-      startAngle: -135,
-      endAngle: 135,
-      track: {
-        background: "#333",
-        startAngle: -135,
-        endAngle: 135,
-      },
-      dataLabels: {
-        name: {
-          show: true,
-        },
-        value: {
-          fontSize: "30px",
-          show: true,
-          formatter: function (altitudeValue) {
-            return altitudeValue + "m";
-          },
-        },
-      },
-    },
-  },
-  fill: {
-    type: "gradient",
-    gradient: {
-      shade: "dark",
-      type: "horizontal",
-      gradientToColors: ["#87D4F9"],
-      stops: [0, 100],
-    },
-  },
-  stroke: {
-    lineCap: "butt",
-  },
-  labels: ["Altitude"],
-};
-
-var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
-chart2.render();
-
-var options3 = {
-  chart: {
-    height: 280,
-    type: "radialBar",
-  },
-  series: [0],
-  colors: ["#A312D2"],
-  plotOptions: {
-    radialBar: {
-      dataLabels: {
-        name: {
-          show: false,
-        },
-        value: {
-          fontSize: "20px",
-          show: true,
-          formatter: function (pressureValue) {
-            return pressureValue + "kpa";
-          },
-        },
-      },
-    },
-  },
-  fill: {
-    type: "gradient",
-    gradient: {
-      shade: "dark",
-      type: "horizontal",
-      gradientToColors: ["#87D4F9"],
-      stops: [0, 100],
-    },
-  },
-  stroke: {
-    lineCap: "butt",
-  },
-  labels: ["Pressure"],
-};
-
-var chart3 = new ApexCharts(document.querySelector("#chart3"), options3);
-chart3.render();
-function updateChart() {
-  $.getJSON("BME280.json", function (rez) {
-    return rez.json();
-  }).then((dat) => {
-    dat.forEach((ValElement) => {
-      chart.updateSeries([ValElement.Humidity.toString().replace(/\%/g, "")]);
-      chart3.updateSeries([
-        ValElement.pressure.toString().replace(/\hPa/g, ""),
-      ]);
-      chart2.updateSeries([ValElement.altitude.toString().replace(/\m/g, "")]);
-    });
-  });
-}
-
-setInterval(updateChart, 200);
-*/
--- json.php
+++ json.php
@@ -56,4 +56,58 @@ if($queryOBC){
 }
 
 
+
+// code for Gyro Sensor
+
+if(isset($_POST['submitGyro'])){
+  //open the json file
+  $dataOBC = file_get_contents('gyroSensor.json');
+  $dataOBC_array = json_decode($dataOBC);
+  
+  //dataOBC in out POST
+  $inputOBC = array(
+      'acceleration' => "X: ".$_POST['accelerationX']. ", Y: ".$_POST['accelerationY'].", Z: ".$_POST['accelerationZ'],
+      'gyroscope' => "X: ".$_POST['accelerationX']. ", Y: ".$_POST['accelerationY'].", Z: ".$_POST['gyroscopeZ'],
+  );
+  
+  //append the input to our array
+  $dataOBC_array[] = $inputOBC;
+  //encode back to json
+  $dataOBC = json_encode($dataOBC_array, JSON_PRETTY_PRINT);
+  $queryGyro = file_put_contents('gyroSensor.json', $dataOBC);
+  if($queryGyro){ 
+    echo 200;
+  }else{
+    echo 404;
+  }
+  }
+  
+  // code for GPS Sensor
+
+if(isset($_POST['submitGPS'])){
+  //open the json file
+  $dataOBC = file_get_contents('GPS.json');
+  $dataOBC_array = json_decode($dataOBC);
+  
+  //dataOBC in out POST
+  $inputOBC = array(
+      'Latitude' => $_POST['gpsLatitude']."&deg;".$_POST['gpsLatitudeN']."'N",
+      'Longitude' => $_POST['gpsLongitude']."&deg;".$_POST['gpsLongitudeN']."'E",
+      'gpsAltitude' => $_POST['gpsAltitude']."m",
+      
+  );
+  
+  //append the input to our array
+  $dataOBC_array[] = $inputOBC;
+  //encode back to json
+  $dataOBC = json_encode($dataOBC_array, JSON_PRETTY_PRINT);
+  $queryGPS = file_put_contents('GPS.json', $dataOBC);
+  if($queryGPS){ 
+    echo 200;
+  }else{
+    echo 404;
+  }
+  }
+
+
   ?>
